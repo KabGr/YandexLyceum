1d5f39ac3f1f8c347f5e07b7a947ea5eb5264e7f
Григорий <g.kabakov@inbox.ru>
2021-11-22 20:15:01 +0300

    main

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
+import sqlite3
 import sys
-from random import randint
 
-from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QApplication, QMainWindow
-
-from UI import Ui_MainWindow
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem
 
 
-class MyWidget(QMainWindow, Ui_MainWindow):
+class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
-        self.setFixedSize(400, 430)
-        self.f = []
-        self.pushButton.clicked.connect(self.click)
-
-    def click(self):
-        self.f = randint(1, 200), [randint(0, 255) for _ in range(3)]
-        self.update()
-
-    def paintEvent(self, event):
-        if self.f:
-            qp = QPainter()
-            qp.begin(self)
-            qp.translate(200, 230)
-            qp.setBrush(QColor(*self.f[1]))
-            qp.drawEllipse(-self.f[0], -self.f[0], self.f[0] * 2, self.f[0] * 2)
-            qp.end()
+        self.tableWidget = QTableWidget()
+        uic.loadUi('main.ui', self)
+        con = sqlite3.connect('films_db.sqlite')
+        cur = con.cursor()
+        res = cur.execute('select * from coffee').fetchall()
+        self.tableWidget.setRowCount(len(res))
+        self.tableWidget.setColumnCount(len(res[0]) if res else 0)
+        self.tableWidget.setHorizontalHeaderLabels(
+            i[0] for i in con.execute('select * from coffee').description)
+        for i, line in enumerate(res):
+            for j, val in enumerate(line):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+        self.tableWidget.resizeColumnsToContents()
+        con.close()
 
 
 if __name__ == '__main__':
